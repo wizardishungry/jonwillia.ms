b4b1156bd4b175c9deface623e7a16235ec447a7
Jon Williams <jonathan.williams@gmail.com>
2008-07-01 23:49:07 +0000

    Massive update

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
     $taglines=Array(
         'The Wizard of IDDQD',
         'The Ancestral Home of the Croissanwich<sup>®</sup>',
-        'Last night a tiny sandwich saved my life',
+        'Valhalla, I am punning!',
     );
     $withlines=Array(
         'Surfing the Internet with',
         'Reticulating splines with',
         'Going out to dinner and picking up the tab with',
+        'Delivering the Groceries at 138 bpm via',
     );
     $tagline=$taglines[array_rand($taglines)];
     $withline=$withlines[array_rand($withlines)];
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en"><head>
-
-
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title><?php echo $withline; ?> Wizardishungry</title>
-<link rel="stylesheet" href="include/styles.css" type="text/css">
+<link rel="stylesheet" href="include/style.css" type="text/css">
+<link rel="alternate" type="application/rss+xml" title="Blog" href="http://wizardishungry.wordpress.com/feed" />
+<link rel="alternate" title="FriendFeed" href="http://friendfeed.com/wizardishungry?format=atom" type="application/atom+xml"/>
+<link rel="openid.server" href="https://api.screenname.aol.com/auth/openidServer" />
+<link rel="openid.delegate" href="http://openid.aol.com/wizardishungry" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<meta http-equiv="imagetoolbar" content="no" />
 </head><body>
-
+<?php if(FALSE): ?>
+<div id="leftcolumn">
+<!-- a place for secrets -->
+</div>
+<?php endif; ?>
 <table id="pagetable">
 <tbody><tr>
 <td id="content">
@@ -28,152 +38,137 @@
 <h1 class="bar">&nbsp;</h1>
 <h1 class="tagline"><?php echo $tagline ?></h1>
 <h1 class="title1"><?php echo $withline ?>…</h1>
-<img class="splash" width="576" height="576" src="include/splash.jpg">
+<img class="splash" width="576" height="576" src="include/splash.jpg" alt="O'Reilly Style image of an adorable corgi">
 <h1 class="title2">Wizardishungry</h1>
 
 
 <!-- CONTENT STARTS -->
 
-<script type="text/javascript" src="http://del.icio.us/feeds/js/wizardishungry?tags;extended;count=25;title=del.icio.us;name;showadd"></script>
-<noscript><a href="http://del.icio.us/wizardishungry">del.icio.us</a></noscript>
-
 
 <h2>Introduction</h2>
 
-<p>This is the online home of <span style="text-decoration: underline;">Version
-   Control with Subversion</span>,
-   a <a href="http://svnbook.red-bean.com/en/1.1/ape.html">free</a>
-   book about <a href="http://subversion.tigris.org/">Subversion</a>, a
-   new version control system designed to supplant CVS.  As you may
-   have guessed from the layout of this page, this book is published by
-   <a href="http://www.oreilly.com/catalog/0596004486/">O'Reilly Media</a>.</p>
+<p>This is the somewhat transitional online home of <a href="mailto:jon@wizardishungry.com">Jon Williams</a>
+   until he gets his shit together and puts stuff online in a cohesive, comprehensive and visually pleasing
+   way. Until then this will have to serve as a catch-all for everything related to my online presense.</p>
 
-<p>This is a place to
-   read HTML and PDF versions of the book (although you can certainly
-   <a href="http://svnbook.red-bean.com/buy/">buy</a> a copy if you'd like to).  We'll do our best
-   to keep the site up-to-date.  As Subversion development continues, the
-   product will continue to grow new features, and we plan to continue
-   documenting those changes.</p>
+<h2>What is there to do on this webpage?</a></h2>
 
-<p style="background-color: yellow;"><strong>WE NEED YOUR
-   HELP!</strong> Ben, Fitz and Mike are wrapping up final edits for
-   the production of <span style="text-decoration: underline;">Version
-   Control with Subversion, Second Edition</span>.  Your technical
-   review of the book would be <em>most</em> helpful!  Please see <a href="http://www.red-bean.com/rbwiki/OnePointFiveTechReview">http://www.red-bean.com/rbwiki/OnePointFiveTechReview</a> for how
-   you can help.</p>
+<p>You probably want to go look at <a href="http://wizardishungry.wordpress.com/">my blog</a> until I get
+   everything else sorted out and relinked here. There's some other stuff that acquantences of mine
+   might like to poke around at below.</p>
 
-<h2>Online Versions of the Book</h2>
+<h2>What have you been up to lately?</h2>
 
-<p>Here are the latest versions of the book which are available online:</p>
+<p>Usually, <a href='#delicious'>my del.icio.us</a> is a pretty good indicator of what I've been thinking about. I've been doing a
+   lot of work in <a href="http://www.symfony-project.org/">Symfony</a> for the past two years and continue
+   to do so. Lately my work interests have expanded a little bit, but more on that later.</p>
+
+<h2>Is that a Corgi?</h2>
+
+<p>Yes, that is a <a href="http://en.wikipedia.org/wiki/Pembroke_Welsh_Corgi">Pembroke Welsh Corgi</a>. I
+   had two of them growing up and my parents have a pair back home. I remain
+   <a href="http://www.flickr.com/photos/wizardishungry/sets/72157594187664253/">a big fan</a>.</p>
+
+<a name="delicious"></a><script type="text/javascript" src="http://del.icio.us/feeds/js/WIZARDISHUNGRY?extended;count=10;title=My%20del.icio.us;bullet=%E2%80%A2;icon=s;name;showadd"></script>
+<noscript><a href="http://del.icio.us/WIZARDISHUNGRY">My del.icio.us</a></noscript>
+
+
+<h2>OMG more Widgets!<h2>
+<a href="http://www.facebook.com/people/Jon_Williams/3700497" title="Jon Williams's Facebook profile" target=_TOP><img src="http://badge.facebook.com/badge/3700497.302.1982974835.png" border=0 alt="Jon Williams's Facebook profile"></a>
+<!-- Start of Flickr Badge -->
+<style type="text/css">
+#flickr_badge_source_txt {padding:0; font: 11px Arial, Helvetica, Sans serif; color:#666666;}
+#flickr_badge_icon {display:block !important; margin:0 !important; border: 1px solid rgb(0, 0, 0) !important;}
+#flickr_icon_td {padding:0 5px 0 0 !important;}
+.flickr_badge_image {text-align:center !important;}
+.flickr_badge_image img {border: 1px solid black !important;}
+#flickr_www {display:block; text-align:left; padding:0 10px 0 10px !important; font: 11px Arial, Helvetica, Sans serif !important; color:#3993ff !important;}
+#flickr_badge_uber_wrapper a:hover,
+#flickr_badge_uber_wrapper a:link,
+#flickr_badge_uber_wrapper a:active,
+#flickr_badge_uber_wrapper a:visited {text-decoration:none !important; background:inherit !important;color:#3993ff;}
+#flickr_badge_wrapper {}
+#flickr_badge_source {padding:0 !important; font: 11px Arial, Helvetica, Sans serif !important; color:#666666 !important;}
+</style>
+<table id="flickr_badge_uber_wrapper" cellpadding="0" cellspacing="10" border="0"><tr><td><a href="http://www.flickr.com" id="flickr_www">www.<strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong>.com</a><table cellpadding="0" cellspacing="10" border="0" id="flickr_badge_wrapper">
+<tr>
+<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?show_name=1&count=3&display=latest&size=t&layout=h&source=user&user=80228949%40N00"></script>
+<td id="flickr_badge_source" valign="center" align="center">
+<table cellpadding="0" cellspacing="0" border="0"><tr>
+<td width="10" id="flickr_icon_td"><a href="http://www.flickr.com/photos/wizardishungry/"><img id="flickr_badge_icon" alt="WIZARDISHUNGRY's items" src="http://farm1.static.flickr.com/56/buddyicons/80228949@N00.jpg?1198015831#80228949@N00" align="left" width="48" height="48"></a></td>
+<td id="flickr_badge_source_txt"><nobr>Go to</nobr> <a href="http://www.flickr.com/photos/wizardishungry/">WIZARDISHUNGRY's photostream</a></td>
+</tr></table>
+</td>
+</tr>
+</table>
+</td></tr></table>
+<!-- End of Flickr Badge -->
+
+
+<h2>Things That Formerly Existed</h2>
+
+<p>Thanks to the wonders of the <a href="http://www.archive.org/web/web.php">Wayback Machine</a>, you can see a 
+   few interesting snapshots of this page:</p>
 
 <div id="versionlist" class="versionlist">
 
-<h3>For Subversion 1.4</h3>
+<h3>Blog frontpages layout</h3>
 
+<p>All of the content that was available here was moved to a
+   <a href="http://wizardishungry.wordpress.com/">WordPress.com blog</a> after I decided it was too tedious to
+   maintain a blog install for what was eseentially a infrequently-updated “Tumblog” (I do not enjoy writing)
+   and <a href="http://en.wikipedia.org/wiki/Grey_hat">grey hat</a> Technorati optimzation experiment. I
+   <strong>promise</strong> I'll get the "Oregon Trail of Animated Gifs" up on my site somewhere.</p>
+
+<h3>Older static pages</h3>
 <ul>
-<li>View the <a href="http://svnbook.red-bean.com/en/1.4/index.html">multiple-page HTML edition</a>
-    of the book.</li>
-<li>View the <a href="http://svnbook.red-bean.com/en/1.4/svn-book.html">single-page HTML edition</a>
-    of the book (1.2MB).</li>
-<li>View the <a href="http://svnbook.red-bean.com/en/1.4/svn-book.pdf">PDF edition</a> of the book
-    (1.4MB).</li>
-<li>Download the <a href="http://svnbook.red-bean.com/en/1.4/svn-book-html.tar.bz2">single-page HTML edition</a> of the book in a <tt>.tar.bz2</tt> archive
-    (~364KB).</li>
-<li>Download the <a href="http://svnbook.red-bean.com/en/1.4/svn-book-html-chunk.tar.bz2">multiple-page HTML edition</a> of the book in a <tt>.tar.bz2</tt> archive
-    (~380KB).</li>
-<li>View the book's <a href="http://svnbook.red-bean.com/trac/browser/tags/en-1.4-final/src/en/book/">DocBook 
-    XML sources</a>.</li>
+<li><a href="http://web.archive.org/web/20061014044456/http://wizardishungry.com/">Oct 2006</a> Last Static layout
+    (javascript+color+css intensive)</li>
+<li><a href="http://web.archive.org/web/20061014044456/http://wizardishungry.com/">Nov 2005</a> Classic bigsplash with
+    alpha-channel png</li>
+<li><a href="http://web.archive.org/web/20040722190452/http://wizardishungry.com/">Jul 2004</a> Classic layout with different
+    glyphs</a> choice of random characters was probably influenced by the font support in my then-current Linux
+    distribution.
+    <ul>
+    <li><a href="http://web.archive.org/web/20040322034535/http://wizardishungry.com/">Broken seizure inducing
+    subpage</a> (a little better in <a href="http://web.archive.org/web/20041009154036/wizardishungry.com/hotcar.html">
+    Oct 2004</a>)</li>
+    </ul></li>
+<li><a href="http://web.archive.org/web/20040322034535/http://wizardishungry.com/">Mar 2004</a> Broken version of the classic
+    blink+huge png layout</li>
+<li><a href="http://web.archive.org/web/20030805160825/wizardishungry.com/">Aug 2003</a> Slightly more annoying CSS</li>
+<li><a href="http://web.archive.org/web/20030424191836/http://wizardishungry.com/">Apr 2003</a> DHTML+CSS+&gt;BLINK&gt;</li>
+<li><a href="http://web.archive.org/web/20030217055846/http://wizardishungry.com/">Feb 2003</a> Early synth experiment mp3
+    page</li>
 </ul>
+<h3>My pages etc. predating this domain</h3>
 
-<h3>Nightly Build (for Subversion 1.5)</h3>
-<p>Please bear in mind that these versions are works-in-progress: if you
-bookmark or link to specific sections, those links may be invalidated by
-continuing development. If you need a link that can be reasonably expected to
-remain stable for years to come, link to one of the completed editions
-below.</p>
-
-<ul>
-<li>View the <a href="http://svnbook.red-bean.com/nightly/en/index.html">multiple-page HTML edition</a>
-    of the book.</li>
-<li>View the <a href="http://svnbook.red-bean.com/nightly/en/svn-book.html">single-page HTML edition</a>
-    of the book (1.1MB).</li>
-<li>View the <a href="http://svnbook.red-bean.com/nightly/en/svn-book.pdf">PDF edition</a> of the book
-    (1.3MB).</li>
-<li>Download the <a href="http://svnbook.red-bean.com/nightly/en/svn-book-html.tar.bz2">single-page HTML edition</a> of the book in a <tt>.tar.bz2</tt> archive
-    (~350KB).</li>
-<li>Download the <a href="http://svnbook.red-bean.com/nightly/en/svn-book-html-chunk.tar.bz2">multiple-page HTML edition</a> of the book in a <tt>.tar.bz2</tt> archive
-    (~350KB).</li>
-<li>View the book's <a href="http://svnbook.red-bean.com/trac/browser/trunk/src/en/book/">DocBook 
-    XML sources</a>.</li>
-</ul>
+<p>Like you'd want <a href="http://wizardishungry.wordpress.com/2007/01/13/hello_my_future_girlfriend_boy_surrives_transition_to_web_20_alive_and_well_on_myspace/">
+   embarassing shit from when you were young</a> available for future employers and spouses?</a>
 
 </div>
-
-<p>You can also find older versions of the book (which we suspect are
-   no longer of much interest to most folks) <a href="http://svnbook.red-bean.com/old-versions.html">here</a>.</p>
-
-<h2>Feedback/Contributing</h2>
-
-<p>For feedback on the book or this website, contact
-   <tt>svnbook-dev@red-bean.com</tt>
-   [<a href="http://www.red-bean.com/mailman/listinfo/svnbook-dev">listinfo</a>].
-   If you have spotted errors in the book (O'Reilly's hardcopy or
-   otherwise), please do the following things:</p>
-
-<ol>
-<li>Check our <a href="http://svnbook.red-bean.com/trac/query?status=new&amp;status=assigned&amp;status=reopened">issue tracker</a> to see if someone else has already reported the
-    same problem.  If so, there's nothing else to do, unless you wish
-    to contribute a patch which fixes the problem (see below).</li>
-<li>Check the XML sources to see if the problem still exists.  You can
-    grab these using Subversion itself, by checking out the trunk of our
-    Subversion repository at <tt>http://svn.red-bean.com/svnbook/trunk/</tt>.
-    If the problem is present in the latest book sources, please
-    report the problem to the mailing list above.</li>
-<li>If the problem is in the published book, check O'Reilly's <a href="http://www.oreilly.com/catalog/0596004486/errata/">errata
-    page</a> for the book, and report the error there if it hasn't
-    already been reported.</li>
-</ol>
-
-<p>Reports of errors in the book are always welcome.  Reports of
-   errors in the book which are accompanied by a suggested fix for the
-   problem are even better.  For technical fixes (spelling, grammar,
-   markup, etc.), just include with your error-reporting email a patch
-   against the XML sources (and include the word <tt>[PATCH]</tt> in
-   the subject line).  For more subjective concerns about the tone or
-   comprehensibility of a passage, it's best just raise that topic on
-   the mailing list.</p>
-
-<h2>Translations</h2>
-
-<p>This book has been (or is being) translated to other languages.
-   Use the navigation menu at the bottom of the page to select a
-   different language.  From each translated page you can get
-   instructions on obtaining the translated book (or a
-   work-in-progress snapshot if it is not finished yet).  Note that
-   the English version is the master from which all translations
-   derive, and if you have any comments about a translation you should
-   contact that translation's authors.</p>
-
-<!-- NON-ENGLISH TRANSLATION-SPECIFIC STUFF BEGINS -->
-
-<!-- NON-ENGLISH TRANSLATION-SPECIFIC STUFF ENDS -->
-
-<h2>Are Those Turtles?</h2>
-
-<p>Why, yes, they are, indeed, turtles.  That's the animal chosen by
-   the publisher for our book cover.  And before you ask us, "Why?"
-   — we don't really know.  It's cool, and our wives are pleased
-   that at least something "icky" wasn't chosen to represent
-   Subversion.</p>
-
-<!-- CONTENT ENDS -->
-
+<p>Well I finally got off my ass and updated this; hopefully things will be more cohesive once I move to my
+   “new” Solaris machine. Till then.</p>
 <h1 class="authorlist"><a href="mailto:jon@wizardishungry.com">Jon Williams</a></h1>
-
 <hr>
-<p>HTML/CSS Ripped-off with love via <a href="http://creativecommons.org/licenses/by/2.0/" rel="license">
-   cc attribution license 2.0</a> from <a href="http://svnbook.red-bean.com/">svn-book</a>.</p>
+
+<p>HTML&amp;CSS stolen (with love) from <a href="http://svnbook.red-bean.com/">svn-book</a>
+(<a href="http://creativecommons.org/licenses/by/2.0/" rel="license">creative commons license 2.0 [attrib]</a>)</p>
+
+<p>Last-updated: <?php
+$fs=stat(__FILE__);
+$time=$fs['mtime'];
+echo date(DATE_RFC2822,$time)
+?></p>
+<span id="bling">
+Widescreen Rules!
+</span>
 </td>
 </tr>
 </tbody></table>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+<script type="text/javascript">
+_uacct = "UA-938181-1";
+urchinTracker();
+</script>
 </body></html>
