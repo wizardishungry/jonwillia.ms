188a78dec1a2a97f52ce8474611907dde5c56fbd
Jon Williams <jonathan.williams@gmail.com>
2008-12-29 17:51:54 -0500

    temp disable pw

--- post-receive.php
+++ post-receive.php
@@ -16,12 +16,12 @@ $my_git='/usr/local/bin/git';
 
 function hide($e)
 {
-    die($e);
+    trigger_error($e);
     header("Status: 404 Not Found");
     exit();
 }
     if(!isset($_POST['payload'])) { hide('payloaf'); }
-    if(@$_GET['secret']!=$my_secret) { hide('sec'); }
+    //    if(@$_GET['secret']!=$my_secret) { hide('sec'); }
     //$payload=json_decode($_POST['payload']);
     echo shell_exec("cd $my_path && sudo -u  $my_user $my_git pull");
 
