1c2cdd76dcbd13a25269d6d168c55ab6b2106761
Jon Williams <jonathan.williams@gmail.com>
2008-12-29 17:37:54 -0500

    yay

--- post-receive.php
+++ post-receive.php
@@ -14,14 +14,16 @@ $my_path='/var/www/htdocs/wizardishungry.com/';
 $my_user='jon';
 $my_git='/usr/local/bin/git';
 
-function hide()
+function hide($e)
 {
+    die($e);
     header("Status: 404 Not Found");
     exit();
 }
-    if(!isset($_POST['payload'])) { hide(); }
-    if(@$_GET['secret']!=$my_secret) { hide(); }
+    if(!isset($_POST['payload'])) { hide('payloaf'); }
+    if(@$_GET['secret']!=$my_secret) { hide('sec'); }
     //$payload=json_decode($_POST['payload']);
     echo shell_exec("cd $my_path && sudo -u  $my_user $my_git pull");
 
+    hide('yay'); 
 ?>
