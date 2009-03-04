62a3bcb264ff7896c69e86d241a6ef3b7131394e
Jon Williams <jonathan.williams@gmail.com>
2009-03-04 15:35:19 -0500

    Github fix0r

--- m.gif/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('../cookie.php');
-header("Pragma: no-cache");
-national_treasure_starring_nicholas_cage(3);
-header('Location: http://www.wizardishungry.com/iiam-busted.gif',TRUE,302);
-?>
--- w.jpg/index.php
+++
@@ -1,7 +0,0 @@
-<?
-require_once('../cookie.php');
-//header("Pragma: no-cache");
-header('Content-Type: image/jpeg');
-national_treasure_starring_nicholas_cage(3);
-readfile("/var/www/htdocs/wizardishungry.com/p.jpg")
-?>
